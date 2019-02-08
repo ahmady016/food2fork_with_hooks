48ea088cb498f27a4de9a661b49855c8b5f029ad
ahmady016 <ahmady09@gmail.com>
2019-02-08 09:08:50 +0200

    basic content: add app-intro

--- src/App.js
+++ src/App.js
@@ -11,13 +11,18 @@ export default function App() {
             <img className="app-logo" src={logo} alt="Food2Fork" />
           </a>
           <form className="form-inline">
-            <input className="form-control mr-sm-2 w-75" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-warning my-2 my-sm-0" type="submit">Search</button>
+            <input className="form-control form-control-lg mr-sm-2 w-75" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-outline-warning btn-lg my-2 my-sm-0" type="submit">Search</button>
           </form>
         </nav>
       </header>
       <div className="container">
-        food result ...
+        <main className="intro d-flex flex-column align-items-center justify-content-around">
+          <img className="app-logo" src={logo} alt="Food2Fork" />
+          <span>Food2Fork makes it easy to find great recipes.</span>
+          <span>Search by name or ingredients.</span>
+          <span>Join to save your favorites for later.</span>
+        </main>
       </div>
     </>
   )
