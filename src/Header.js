6a799a81f7efed969b480297845b11aeda7f9059
ahmady016 <ahmady09@gmail.com>
2019-02-09 23:39:40 +0200

    Refactor: to doSeach function to handle form submit

--- src/Header.js
+++ src/Header.js
@@ -4,25 +4,26 @@ import logo from './food2fork-logo.png'
 
 export default function Header() {
   const { setError, query, setQuery, setRecipes } = useRecipes();
+  const doSearch = (e) => {
+    e.preventDefault();
+    fetchData({ query, setRecipes, setError });
+  }
   return (
     <header>
       <nav className="navbar navbar-expand-lg bg-dark d-flex justify-content-between align-items-center">
         <a className="navbar-brand text-light" href="#!">
           <img className="app-logo" src={logo} alt="Food2Fork" />
         </a>
-        <form className="form-inline" onSubmit={ e => {
-          e.preventDefault();
-          fetchData({ query, setRecipes, setError });
-        }}>
+        <form className="form-inline" onSubmit={doSearch}>
           <input className="form-control form-control-lg mr-sm-2 w-75"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-                value={query}
-                onChange={({ target: { value } }) => setQuery(value)} />
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+            value={query}
+            onChange={({ target: { value } }) => setQuery(value)} />
           <button className="btn btn-outline-warning btn-lg my-2 my-sm-0" type="submit">
             Search
-            </button>
+          </button>
         </form>
       </nav>
     </header>
